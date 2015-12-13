365fc22280995120ed9a8c1c6b2b256ea8bb7813
kusakata <shohei@kusakata.com>
2015-12-13 22:02:39 +0900

    Unify simple pages

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var about = require('./routes/about');
-var svn = require('./routes/svn');
+var simple = require('./routes/simple');
 
 var app = express();
 
@@ -24,8 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/about', about);
-app.use('/svn', svn);
+
+app.use('/about', simple);
+app.use('/svn', simple);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/simple.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+	var url = req.originalUrl.split("/").join("");
+	switch(url) {
+		case "about":
+			var stitle = "Arch Linux について";
+			break;
+		case "svn":
+			var stitle = "SVN リポジトリ";
+			break;
+	}
+	res.render(url, {
+		title: stitle,
+		selected: "anb-home"
+	});
+});
+
+module.exports = router;
