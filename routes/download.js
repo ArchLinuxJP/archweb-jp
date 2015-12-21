e9f9d7d8289631cfca94aaa198a3e66ff6d992d9
kusakata <shohei@kusakata.com>
2015-12-21 20:46:59 +0900

    import lastest iso info

--- routes/download.js
+++ routes/download.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
 	res.render("download", {
 		title: "ダウンロード",
-		selected: "anb-download"
+		selected: "anb-download",
+		isoinfo: JSON.parse(fs.readFileSync("admin/lastiso.json", "utf8"))
 	});
 });
 
