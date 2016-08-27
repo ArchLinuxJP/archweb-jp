7e468cb519f67c290755e3201bee1974d2fdb830
kusakata <shohei@kusakata.com>
2016-08-27 17:34:47 +0900

    wiki table fix

--- public/javascripts/archweb.js
+++ public/javascripts/archweb.js
@@ -5,3 +5,18 @@ document.getElementById("archnavbaricon").onclick = function() {
 	document.getElementById("archnavbar").style.cssText = "margin-left:0 !important;overflow:auto !important;white-space:nowrap !important;";
 	document.getElementById("archnavbarmenu").style.display="block";
 }
+
+// wiki table
+window.onload = function() {
+	if(location.href.indexOf("wiki.archlinuxjp.org") != -1) {
+		var tables = document.getElementsByTagName("table");
+		for(var i = 0; i < tables.length; i++) {
+			if(tables[i].className.indexOf("wikitable") != -1) {
+				tables[i].outerHTML = '<div style="overflow-x:auto;white-space:nowrap;">' + tables[i].outerHTML + "</div>";
+				if(tables[i].getElementsByTagName("caption").length > 0) {
+					tables[i].getElementsByTagName("caption")[0].style.textAlign = "left";
+				}
+			}
+		}
+	}
+}
