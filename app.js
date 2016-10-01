29d0152336ae02d6310190ed2ff4ff6f3a5e0f93
kusakata <shohei@kusakata.com>
2016-10-01 23:08:05 +0900

    AMP & i18n

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var i18n = require('i18n');
 
 var routes = require('./routes/index');
 var article = require('./routes/article');
@@ -24,16 +25,26 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// i18n setup
+i18n.configure({
+	locales: ['en', 'ja'],
+	defaultLocale: 'ja',
+	queryParameter: 'lang',
+	directory: __dirname + '/admin/translate'
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(i18n.init);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+app.use('/news/:news([0-9a-zA-Z\-]+)/amp', article);
 app.use('/news/:news([0-9a-zA-Z\-]+)', article);
 app.use('/news', news);
 
--- routes/article.js
+++ routes/article.js
@@ -8,17 +8,19 @@ var db = new sqlite3.Database("admin/index.sqlite3");
 router.get("/", function(req, res, next) {
 	db.serialize(function() {
 		var request = req.originalUrl.split("/")[2];
+		var amp = (req.originalUrl.split("/").length > 4) ? "amp" : "";
 		db.each("SELECT * FROM news WHERE url = '" + request + "'", function(err, row){
 			if (!err) {
 				var html = fs.readFileSync("views/news/" + row.url + ".html", "utf-8");
-				res.render("article", {
+				res.render(amp + "article", {
 					title: "ニュース: " + row.headline,
 					created: row.created,
 					modified: row.modified,
 					author: row.author,
 					url: row.url,
 					body: html,
-					selected: "anb-home"
+					selected: "anb-home",
+					amp: "amp"
 				});
 			}
 		}, function(err, count){
