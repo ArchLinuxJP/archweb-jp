77161b3d967a6e2b5ebef8822f6836fe2b534eea
kusakata <shohei@kusakata.com>
2018-01-01 00:40:03 +0900

    fix paging bug

--- routes/news.js
+++ routes/news.js
@@ -15,19 +15,31 @@ router.get("/", function(req, res, next) {
 		
 		db.all("SELECT *, (SELECT COUNT(url) FROM news) AS count FROM news" + limit, function(err, rows){
 			if (!err) {
-				var total = Math.ceil(rows[0].count * 0.02);
-				var nextp = (page < total) ? page + 1 : 0;
-				var prevp = (page > 1) ? page - 1 : 0;
-				
-				res.render("news", {
-					title: "ニュース",
-					news: rows,
-					selected: "anb-home",
-					page: page,
-					total: total,
-					nextp: nextp,
-					prevp: prevp
-				});
+				if(rows.length == 0){
+					res.render("news", {
+						title: "ニュース",
+						news: new Array({count:0}),
+						selected: "anb-home",
+						page: 1,
+						total: 1,
+						nextp: 0,
+						prevp: 0
+					});
+				}else{
+					var total = Math.ceil(rows[0].count * 0.02);
+					var nextp = (page < total) ? page + 1 : 0;
+					var prevp = (page > 1) ? page - 1 : 0;
+					
+					res.render("news", {
+						title: "ニュース",
+						news: rows,
+						selected: "anb-home",
+						page: page,
+						total: total,
+						nextp: nextp,
+						prevp: prevp
+					});
+				}
 			}else{
 				next(err);
 			}
