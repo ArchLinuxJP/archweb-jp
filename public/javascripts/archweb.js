10ca68b23ed9a2b9d835248a8dab859f4fe4ea4c
kusakata <shohei@kusakata.com>
2018-01-19 00:55:42 +0900

    archweb.js style fix

--- public/javascripts/archweb.js
+++ public/javascripts/archweb.js
@@ -2,7 +2,7 @@
 document.getElementById("archnavbaricon").onclick = function() {
 	document.getElementById("archnavbarlogo").style.display = "none";
 	document.getElementById("archnavbaricon").style.cssText = "display:none !important;";
-	document.getElementById("archnavbar").style.cssText = "margin-left:0 !important;overflow:auto !important;white-space:nowrap !important;";
+	document.getElementById("archnavbar").style.cssText = "margin-left:0 !important;overflow-x:auto !important;overflow-y:hidden !important;white-space:nowrap !important;";
 	document.getElementById("archnavbarmenu").style.display="block";
 }
 
