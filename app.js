c85d10e6d079e116393dc87de44c8ec36187b0e8
kusakata <shohei@kusakata.com>
2018-02-05 22:29:16 +0900

    migrate from jade to pug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // i18n setup
 i18n.configure({
